147e25905926cb7e778b3b677fe30cd7c765c89d
Brandon <brodrig8326@student.gwinnetttech.edu>
2022-10-25 01:44:48 -0400

    fixing images

--- components/Body.js
+++ components/Body.js
@@ -64,7 +64,7 @@ function Body({ chooseTrack, spotifyApi }) {
   return (
     <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
       <Search search={search} setSearch={setSearch} />
-      <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4">
+      <div className="grid overflow-y-scroll scrollbar-hide h-[28rem] py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-[6rem] gap-y-8 p-4">
         {searchResults.length === 0
           ? newReleases
               .slice(0, 4)
